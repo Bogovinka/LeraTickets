81d6f9233105d968dd98eba5c2a107f85d99e013
Bogovinka <77607705+Bogovinka@users.noreply.github.com>
2022-02-17 22:19:32 +0500

    edit

---
+++ TicketBuy/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicketBuy
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginB_Click(object sender, RoutedEventArgs e)
+        {
+            Menu m = new Menu();
+            m.Show();
+            Close();
+        }
+
+        private void RegB_Click(object sender, RoutedEventArgs e)
+        {
+            Reg r = new Reg();
+            r.Show();
+            Close();
+        }
+    }
+}
---
+++ TicketBuy/Reg.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TicketBuy
+{
+    /// <summary>
+    /// Логика взаимодействия для Reg.xaml
+    /// </summary>
+    public partial class Reg : Window
+    {
+        Regex hasNumber = new Regex(@"[0-9]+");
+        Regex hasUpperChar = new Regex(@"[A-Z]+");
+        Regex hasUpperCharMini = new Regex(@"[a-z]+");
+        Regex hasMinimum8Chars = new Regex(@".{8,}");
+
+        public Reg()
+        {
+            InitializeComponent();
+        }
+        private void nextB_Click(object sender, RoutedEventArgs e)
+        {
+            if (loginText.Text.Replace(" ", "").Length >= 3)
+            {
+                if (passwordText.Password == passwordText2.Password)
+                {
+                    string pass = passwordText.Password;
+                    int hardPass = 0;
+                    if (hasNumber.IsMatch(pass)) hardPass++;
+                    if (hasUpperChar.IsMatch(pass)) hardPass++;
+                    if (hasUpperCharMini.IsMatch(pass)) hardPass++;
+                    if (hasMinimum8Chars.IsMatch(pass)) hardPass++;
+                    if (hardPass > 1)
+                    {
+                        Menu m = new Menu();
+                        m.Show();
+                    }
+                    else MessageBox.Show("Слишком легкий пароль", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else MessageBox.Show("Пароли не совпадают", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else MessageBox.Show("Логин должен быть минимум 3 символа", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void RegB_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow m = new MainWindow();
+            m.Show();
+            this.Close();
+        }
+
+        private void passwordText_KeyUp(object sender, KeyEventArgs e)
+        {
+            string pass = passwordText.Password;
+            int hardPass = 0;
+            if (hasNumber.IsMatch(pass)) hardPass++;
+            if (hasUpperChar.IsMatch(pass)) hardPass++;
+            if (hasUpperCharMini.IsMatch(pass)) hardPass++;
+            if (hasMinimum8Chars.IsMatch(pass)) hardPass++;
+            if (hardPass <= 1) prov.Background = new SolidColorBrush(Colors.Red);
+            else if (hardPass == 2) prov.Background = new SolidColorBrush(Colors.Orange);
+            else if (hardPass == 3) prov.Background = new SolidColorBrush(Colors.Yellow);
+            else if (hardPass == 4) prov.Background = new SolidColorBrush(Colors.LightGreen);
+        }
+    }
+}
