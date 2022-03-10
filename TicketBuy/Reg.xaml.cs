1c0c47d6c84060b5e7b623ef08118ef58baebe0b
Bogovinka <ma.faker.pro@gmail.com>
2022-03-11 00:30:15 +0500

    RegistrationAdd

--- TicketBuy/Reg.xaml.cs
+++ TicketBuy/Reg.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -25,6 +27,7 @@ namespace TicketBuy
         Regex hasUpperCharMini = new Regex(@"[a-z]+");
         Regex hasMinimum8Chars = new Regex(@".{8,}");
 
+        string pathAuto = @"auto.txt";
         public Reg()
         {
             InitializeComponent();
@@ -41,17 +44,58 @@ namespace TicketBuy
                     if (hasUpperChar.IsMatch(pass)) hardPass++;
                     if (hasUpperCharMini.IsMatch(pass)) hardPass++;
                     if (hasMinimum8Chars.IsMatch(pass)) hardPass++;
+                    WorkBD test = new WorkBD();
                     if (hardPass > 1)
                     {
-                        Menu m = new Menu();
-                        m.Show();
+                        if (!test.userHave(loginText.Text))
+                        {
+                            using (FileStream fs = File.Create(pathAuto))
+                            {
+                                byte[] info = new UTF8Encoding(true).GetBytes($"{loginText.Text}\n{passwordText.Password}");
+                                // Add some information to the file.
+                                fs.Write(info, 0, info.Length);
+                            }
+
+                            // строка подключения к базе данных
+                            string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DatabaseLogins.mdf;Integrated Security=True";
+                            SqlConnection connection = new SqlConnection(connectionString);
+                            connection.Open();
+                            string query = $"INSERT INTO Logins(Login, Password) VALUES ('{loginText.Text.ToString()}', '{passwordText.Password.ToString()}')";
+                            SqlCommand command = new SqlCommand(query, connection);
+                            // выполняем запрос
+                            command.ExecuteNonQuery();
+                            MainWindow m = new MainWindow();
+                            m.Show();
+                            connection.Close();
+
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Такой логин уже есть");
+                        }
                     }
-                    else MessageBox.Show("Слишком легкий пароль", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                    else
+                    {
+                        MessageBox.Show("Слишком легкий пароль");
+                    }
+
+
                 }
-                else MessageBox.Show("Пароли не совпадают", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                else
+                {
+                    MessageBox.Show("Пароли не совпадают");
+                }
+
             }
-            else MessageBox.Show("Логин должен быть минимум 3 символа", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+            {
+                MessageBox.Show("Логин должен быть минимум 3 символа");
+            }
+
         }
+
+
 
         private void RegB_Click(object sender, RoutedEventArgs e)
         {
@@ -60,18 +104,37 @@ namespace TicketBuy
             this.Close();
         }
 
-        private void passwordText_KeyUp(object sender, KeyEventArgs e)
+        private void passwordText_KeyDown(object sender, KeyEventArgs e)
         {
             string pass = passwordText.Password;
             int hardPass = 0;
-            if (hasNumber.IsMatch(pass)) hardPass++;
-            if (hasUpperChar.IsMatch(pass)) hardPass++;
-            if (hasUpperCharMini.IsMatch(pass)) hardPass++;
-            if (hasMinimum8Chars.IsMatch(pass)) hardPass++;
-            if (hardPass <= 1) prov.Background = new SolidColorBrush(Colors.Red);
-            else if (hardPass == 2) prov.Background = new SolidColorBrush(Colors.Orange);
-            else if (hardPass == 3) prov.Background = new SolidColorBrush(Colors.Yellow);
-            else if (hardPass == 4) prov.Background = new SolidColorBrush(Colors.LightGreen);
+            if (hasNumber.IsMatch(pass) && pass.Length >= 4) hardPass++;
+            if (hasUpperChar.IsMatch(pass) && pass.Length >= 4) hardPass++;
+            if (hasUpperCharMini.IsMatch(pass) && pass.Length >= 4) hardPass++;
+            if (hasMinimum8Chars.IsMatch(pass) && pass.Length >= 4) hardPass++;
+            if (hardPass == 0) { prov.Background = new SolidColorBrush(Colors.Gray); prov.Content = "Не подходит"; }
+            else if (hardPass == 1) { prov.Background = new SolidColorBrush(Colors.Red); prov.Content = "Плохой"; }
+            else if (hardPass == 2) { prov.Background = new SolidColorBrush(Colors.Orange); prov.Content = "Нормальный"; }
+            else if (hardPass == 3) { prov.Background = new SolidColorBrush(Colors.Yellow); prov.Content = "Хороший"; }
+            else if (hardPass == 4) { prov.Background = new SolidColorBrush(Colors.LightGreen); prov.Content = "Отличный"; }
+        }
+        private void passV_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            passwordText3.Text = passwordText.Password.ToString();
+            passwordText.Visibility = Visibility.Hidden;
+            passwordText3.Visibility = Visibility.Visible;
+        }
+
+        private void passV_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            passwordText.Visibility = Visibility.Visible;
+            passwordText3.Visibility = Visibility.Hidden;
+        }
+
+        private void passV_MouseLeave(object sender, MouseEventArgs e)
+        {
+            passwordText.Visibility = Visibility.Visible;
+            passwordText3.Visibility = Visibility.Hidden;
         }
     }
 }
---
+++ TicketBuy/WorkBD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketBuy
+{
+    class WorkBD
+    {
+        string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DatabaseLogins.mdf;Integrated Security=True";
+        public bool userHave(string login)
+        {
+            int count = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCom = new SqlCommand($"SELECT Login FROM Logins WHERE Login = '{login}'", connection);
+                connection.Open();
+                SqlDataReader read = sqlCom.ExecuteReader();
+                while (read.Read()) count++;
+                if (count > 0) return true;
+                else return false;
+            }
+        }
+        public bool userHave(string login, string pass)
+        {
+            int count = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCom = new SqlCommand($"SELECT Login FROM Logins WHERE Login = '{login}' AND Password = '{pass}'", connection);
+                connection.Open();
+                SqlDataReader read = sqlCom.ExecuteReader();
+                while (read.Read()) count++;
+                if (count > 0) return true;
+                else return false;
+            }
+        }
+
+      
+    }
+}
